7ce89b91ff9d776a1ef972fe1e49cf74dfebe9c4
Jonny McConnell <jonmccon@gmail.com>
2023-10-24 12:57:07 -0700

    added chroma, build test

--- components/header/logo.tsx
+++ components/header/logo.tsx
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
-import { Link } from "gatsby";
-
 class Logo extends Component {
   render() {
     return (
       <React.Fragment>
         
         <div className="logo">
-          <Link to="/"></Link>
+          
         </div>
       
       </React.Fragment>
