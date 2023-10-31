77495224ea1d68468360a7f783ae85fef1b5394e
jonmccon <jonmccon@gmail.com>
2023-10-30 21:41:58 -0700

    menu add of new typeform

--- components/header/menu-left.tsx
+++ components/header/menu-left.tsx
@@ -8,26 +8,37 @@ export default function MenuLeft() {
 
         <div className="headline-menu-left">  
           
+          <a 
+            data-tf-slider="UR7SpT93" 
+            data-tf-position="right" 
+            data-tf-opacity="100" 
+            data-tf-iframe-props="title=SCD - Intake" 
+            data-tf-transitive-search-params data-tf-medium="snippet" 
+            className="headline-menu--link highlight">
+              Get Listed
+           </a>
+          <script src="//embed.typeform.com/next/embed.js"></script>
+
           {/* <Link
           className="headline-menu--link highlight" 
-          to="/submit">Get Listed
+          href="/submit">Get Listed
           </Link> */}
 
-          <a
+          <Link
             className="headline-menu--link"
             target="_blank"
             href="https://seattlecreative.show/"
           >
             Subscribe to Podcast
-          </a>
+          </Link>
 
-          <a
+          <Link
             className="headline-menu--link"
             target="_blank"
             href="https://discord.gg/SVV4XVCFCj"
           >
             Join Discord
-          </a>
+          </Link>
           
           
         </div>
