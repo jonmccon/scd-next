3dfb9fa53f85fbf463c66008872f792d01a43f79
Jonny McConnell <jonmccon@gmail.com>
2023-12-27 16:39:35 -0800

    fixing css imports, rm table-placeholder and index css

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,4 @@
-import '../app/css/fonts.css' /* All hosted and external fonts */
-import '../app/css/index.css' /* Global styles */
-import '../app/css/episodes.css' /* Episodes styles */
-import '../app/css/300.css' /* Mobile styles */
-import '../app/css/1000.css' /* Desktop styles */
-
+import '../app/globals.css' /* All hosted and external fonts */
  
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
