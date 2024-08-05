f33ccd3580f9e37641f12fc3042e30b1918a93cc
jonmccon <jonmccon@gmail.com>
2024-08-04 17:29:18 -0700

    basic bobble animation

--- components/listing/episodes.tsx
+++ components/listing/episodes.tsx
@@ -2,6 +2,7 @@
 import { useState, useEffect } from 'react';
 import EpisodeListing from './episode-listing'
 import LoadEpisode from '../loadins/load-episode';
+import Image from 'next/image';
 
 export default function Episodes() {
   const [data, setData] = useState(null);
@@ -24,7 +25,29 @@ export default function Episodes() {
 
   return (
     <div id="showContainer">  
+    
+      <div className="bobbleTop">
+        <Image 
+          src={"/bobbles/ep12-bobbleTop.png"} 
+          alt='bobbleTop'
+          width={100}
+          height={100}
+          >
+        </Image>
+      </div>
+      <div className="bobbleBottom">
+        <Image  
+          src={"/bobbles/ep12-bobbleBottom.png"} 
+          alt='bobbleBottom'
+          width={100}
+          height={100}
+          >
+        </Image>
+      </div>
+
       <EpisodeListing episodeQuery={data} />
+
+
     </div>
   );
 }
