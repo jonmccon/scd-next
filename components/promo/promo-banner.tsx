445ad35e081bc3f13e1b779fdee31cbdf5152316
jonmccon <jonmccon@gmail.com>
2025-03-26 19:30:29 -0700

    close

--- components/promo/promo-banner.tsx
+++ components/promo/promo-banner.tsx
@@ -9,39 +9,35 @@ export default function PromoBanner() {
     return (
       <React.Fragment>
         
-        <link href="https://use.fontawesome.com/releases/v5.10.2/css/all.css" rel="stylesheet" />
-
-<div>
+        
+<Link href="http://creativeworks.co/west">  
+    <div>
       <div
-        className="text-center flex flex-col p-4 md:text-left md:flex-row md:items-center md:justify-between md:p-4 bg-purple-100"
+        className="promo-banner bg-yellow-400 text-center flex flex-col p-4 md:text-left md:flex-row md:items-center md:justify-between md:p-4"
         id="messageModal"
       >
-        <div className="text-xl font-semibold">
-          <div className="text-gray-900">
-            Some interesting header.
-            <b className="text-purple-500">Some secondary text</b>
+        <img src="/img/cwcWEST.png" alt="Creative Works Logo" className="w-24 h-24 md:w-32 md:h-32" />
+        
+        <div className="text-lg font-regular">
+          <div className="text-black">
+            Join us at Creative Works May 1st to 3rd, 2025 - 
+            <b> Where Craft Meets Career</b>
           </div>
         </div>
 
         <div className="mt-3 md:mt-0 md:ml-2">
           <button
-            className="inline-block rounded-md text-lg font-semibold py-2 px-4 text-white bg-purple-500"
+            className="inline-block rounded-md text-lg font-semibold py-2 px-4 text-black bg-white hover:bg-yellow-500"
             
           >
-            <i className="fas fa-user"></i> Some Button
-          </button>
-          <button
-            className="inline-block rounded-md text-lg font-semibold py-2 px-4 text-black bg-gray-300"
-            
-          >
-            <i className="fas fa-times"></i>
+            <i className="fas fa-user"></i>Register Now
           </button>
         </div>
       </div>
     </div>
-        <Link href="/">          
+                
         
-        </Link>
+</Link>
 
       </React.Fragment>
     );
